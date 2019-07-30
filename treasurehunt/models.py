b247977e2596e7a1194c9140cad78cf5ab8cdfca
AustinBCole <austin.cole.chileno@gmail.com>
2019-07-30 10:13:13 -0600

    Add notes for class methods, install rest_framework and add boilerplate code for read/write permissions.

--- treasurehunt/models.py
+++ treasurehunt/models.py
@@ -5,3 +5,13 @@ class Map(models.Model):
     team_score = models.IntegerField(default=0)
     map = JSONField()
     team_position = JSONField()
+
+    # Add method that increments team_score by a given value
+    def incrementTeamScore(self, amount):
+        pass
+
+    # Add method that updates map with given values
+
+    # Add method that gives map to user
+
+    # Add method that updates team-member position with given value
